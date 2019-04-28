ab0ca87c5c943ed0b5ecd8a174367cb80fb67438
Glenn Hartmann <glennhartmann85@gmail.com>
2019-04-28 17:29:49 -0400

    Add a ServiceWorker to cache all needed resources so that Wormy can work (single-player) offline!

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// main.js handles service worker registration
+
+// Taken almost verbatin from https://developers.google.com/web/fundamentals/primers/service-workers/
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
